5e53613bfcef4571bf6080bfefb08b3759ac8de7
Jasper Koops <jasperkoops91@gmail.com>
2019-11-17 00:06:00 +0100

    Fixed tua

--- tua.py
+++ tua.py
@@ -2,7 +2,7 @@ import time
 
 from selenium import webdriver
 
-base_url = 'https://live.ntr.nl/nvdp/#/814ca5b5929427ca0d731c0cca023398'
+base_url = 'https://live.ntr.nl/nvdp/#/d221e0e86edd4ad8d140035f8900b00e'
 elem_class = 'btn btn-alt ng-scope'
 
 x = 0
@@ -18,9 +18,9 @@ while x < 1000:
     driver.find_element_by_xpath('//div[contains(text(), "Overslaan")]').click()
     time.sleep(0.2)
     driver.find_element_by_xpath('//div[contains(text(), "Doorgaan")]').click()
-
+    time.sleep(0.5)
     # Check even welke je wilt (1 of 2, geen 0 index)
-    el = driver.find_element_by_xpath('//span[contains(text(), "Stemmen")][1]')
+    el = driver.find_element_by_xpath('//p[contains(text(), "Pink Floyd")]/following-sibling::span')
 
     driver.execute_script("arguments[0].click();", el)
     driver.delete_all_cookies()
